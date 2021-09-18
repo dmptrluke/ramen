93896561e399e5d25e61a1bd7bcbcfa5ebf8f119
Luke Rogers <luke@dmptr.com>
2021-09-18 20:51:08 +1200

    Increase speed by 50% with hashing improvements.

--- worker.mjs
+++ worker.mjs
@@ -4,17 +4,18 @@ const worker_id = workerData.i + 1;
 
 import BN from 'bn.js';
 
-import { soliditySha3 } from "./dirtyhash.js";
+import web3 from 'web3';
+import sha3 from 'js-sha3';
+
 import { exit } from 'process';
-
 import { sleep } from './util.mjs';
-
 import { randomBytes } from 'crypto';
 
 var mining_target = config.address;
 
 // worker/parent communication
 var state;
+var prefix;
 var difficulty;
 
 var ready = false;
@@ -24,29 +25,36 @@ var halted = false;
 parentPort.on('message', (message) => {
     if (message.topic === 'pause') {
         paused = true;
-    } 
+    }
     if (message.topic === 'resume') {
         paused = false;
-    } 
+    }
     if (message.topic === 'halt') {
         halted = true;
-    } 
+    }
     if (message.topic === 'state') {
         state = message.data;
+
+        prefix = web3.utils.encodePacked(
+            { t: "uint256", v: config.network.chain_id },
+            { t: "bytes32", v: state.entropy }, { t: "address", v: config.network.gem_address },
+            { t: "address", v: mining_target },
+            { t: "uint", v: config.gem_type },
+            { t: "uint", v: state.nonce }
+        );
         difficulty = new BN(2).pow(new BN(256)).div(new BN(state.difficulty));
         ready = true;
     }
 });
 
-function hash(state) {
-    const salt = new BN(randomBytes(32).toString("hex"), 16);
-    const result = new BN(soliditySha3({ t: "uint256", v: config.network.chain_id },
-        { t: "bytes32", v: state.entropy }, { t: "address", v: config.network.gem_address },
-        { t: "address", v: mining_target },
-        { t: "uint", v: config.gem_type },
-        { t: "uint", v: state.nonce },
-        { t: "uint", v: salt }
-    ).slice(2), 16);
+
+function hash() {
+    var salt = new BN(randomBytes(32).toString("hex"), 16);
+
+    const packed = salt.toTwos(256).toString(16);
+    const packed_bytes = web3.utils.hexToBytes(`${prefix}${packed}`);
+
+    const result = new BN(sha3.keccak_256(packed_bytes), 16);
 
     return { salt, result }
 }
@@ -60,19 +68,19 @@ async function work() {
         while (!ready) {
             await sleep(50);
         }
-        var iteration = hash(state);
-        
+        var iteration = hash();
+
         i += 1;
         if (difficulty.gte(iteration.result)) {
-            if (!paused){
+            if (!paused) {
                 // dont send a gem to the parent thread if we are paused
                 // if we do, the transactions will conflict
-                parentPort.postMessage(iteration.salt.toString()); 
+                parentPort.postMessage(iteration.salt.toString());
             }
-            
+
         }
         if (i % 40000 == 0) {
-            const speed = 40000 / (Number((process.hrtime.bigint() - timer))  / 1000000000); 
+            const speed = 40000 / (Number((process.hrtime.bigint() - timer)) / 1000000000);
             console.log(`[${worker_id}] Hashes: ${i}, Speed: ${(speed / 1000).toFixed(1)}KH/s, Difficulty: ${state.difficulty}`);
 
             var timer = process.hrtime.bigint();
